082c45fe19b0768fe202223cfc10fb2d822784f0
Anthony Quantum <anthonyquantum12@gmail.com>
2019-03-08 20:34:36 +0300

    v2

--- scripts.js
+++ scripts.js
@@ -1,75 +1,53 @@
-function switchToggle(event) {
-    var toggler = event.currentTarget;
-    toggler.classList.toggle('progress__toggle_activated');
-}
+var Progress = {
+    switchToggle: function switchToggle(event) {
+        var toggler = event.currentTarget;
+        toggler.classList.toggle('progress__toggle_activated');
+    },
 
-function hideWheel(event) {
-    switchToggle(event);
-    var block = document.getElementsByClassName("progress")[0];
-    block.classList.toggle("progress_hidden");
-}
+    hideWheel: function hideWheel(event) {
+        this.switchToggle(event);
+        var block = document.getElementsByClassName("progress")[0];
+        block.classList.toggle("progress_hidden");
+    },
 
+    animateWheel: function animateWheel(event) {
+        this.switchToggle(event);
+        var block = document.getElementsByClassName("progress")[0];
+        block.classList.toggle("progress_animated");
+    },
 
+    updateWheel: function updateWheel() {
+        var inp = document.getElementsByClassName("progress__value-control")[0];
+        if (inp.value < 0) inp.value = 0;
+        if (inp.value > 100) inp.value = 100;
+        this.drawWheel(inp.value);
+    },
 
-function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
-  
-    return {
-      x: centerX + (radius * Math.cos(angleInRadians)),
-      y: centerY + (radius * Math.sin(angleInRadians))
-    };
-}
+    drawWheel: function drawWheel(length) {
+        if (length == 100) length = 99.99;
+        document.getElementsByClassName("progress__arc")[0].setAttribute("d", this._describeArc(100, 100, 90, 0, length*3.6));
+    },
 
-var wheel = {
-    startPos: 0,
-    length: 0
-};
-var wheelInterval;
+    _polarToCartesian: function _polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+        var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+      
+        return {
+          x: centerX + (radius * Math.cos(angleInRadians)),
+          y: centerY + (radius * Math.sin(angleInRadians))
+        };
+    },
 
-function animateWheel(event) {
-    switchToggle(event);
-    var block = document.getElementsByClassName("progress")[0];
-    if (block.classList.contains("progress_animated")) {
-        block.classList.remove("progress_animated");
-        clearInterval(wheelInterval);
-    } else {
-        block.classList.add("progress_animated");
-        wheelInterval = setInterval(function() {
-            drawWheel();
-            wheel.startPos = Math.ceil(wheel.startPos) + 1;
-            if (wheel.startPos == 100) wheel.startPos = 0;
-        }, 40);
+    _describeArc: function _describeArc(x, y, radius, startAngle, endAngle){
+        var start = this._polarToCartesian(x, y, radius, endAngle);
+        var end = this._polarToCartesian(x, y, radius, startAngle);
+    
+        var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+    
+        var d = [
+            "M", start.x, start.y, 
+            "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+        ].join(" ");
+    
+        return d;       
     }
-}
-
-function updateWheel() {
-    wheel.length = document.getElementsByClassName("progress__value-control")[0].value;
-    drawWheel();
-}
-
-function drawWheel() {
-    var endPos = +wheel.startPos + +wheel.length;
-    if (+wheel.startPos == 100) wheel.startPos = 99.99;
-    if (endPos == 100) endPos = 99.99;
-    var startAngle = +wheel.startPos*3.6;
-    var endAngle = endPos*3.6;
-    document.getElementsByClassName("progress__arc")[0].setAttribute("d", describeArc(100, 100, 90, startAngle, endAngle));
-}
-
-function describeArc(x, y, radius, startAngle, endAngle){
-    var start = polarToCartesian(x, y, radius, endAngle);
-    var end = polarToCartesian(x, y, radius, startAngle);
-
-    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
-
-    var d = [
-        "M", start.x, start.y, 
-        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
-    ].join(" ");
-
-    return d;       
-}
-
-window.onload = function() {
-    updateWheel();
-}
+};
